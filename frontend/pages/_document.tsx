d27b23aada1e4d7b25e92de014a1ff46764d1b24
gabriel oni <dlastdj10@gmail.com>
2023-06-13 16:58:24 +0100

    Modified Navbar

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Navbar from '../components/navbar';
 
 
-export default function Document() {
+function Document() {
   return (
     <Html lang="en">
       <Head />
@@ -14,3 +14,5 @@ export default function Document() {
     </Html>
   );
 }
+
+export default Document;
